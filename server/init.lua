bf6488020cbff2e9fa666ac787230e53ac3983ae
Case <90377400+CaseIRL@users.noreply.github.com>
2024-08-13 11:41:58 +0100

    Update init.lua

--- server/init.lua
+++ server/init.lua
@@ -1,5 +1,15 @@
--- Import utility library
+--- Import utility library
 utils = exports.boii_utils:get_utils()
+
+--- Version check options
+--- @field resource_name: The name of the resource to check, you can set a value here or use the current resource.
+--- @field url_path: The path to your json file.
+--- @field callback: Callback to invoking resource version check details *optional*
+local opts = {
+    resource_name = 'boii_hunting',
+    url_path = 'boiidevelopment/fivem_resource_versions/main/versions.json',
+}
+utils.version.check(opts)
 
 --- @section Global functions
 
